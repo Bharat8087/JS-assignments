1922c2daacc59ae559dafedaecdb92408cd7707a
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-03-16 20:16:52 +0530

    Addition three numbers inside console

--- addition.js
+++ addition.js
@@ -2,8 +2,10 @@ let a = 5;
 let b = 7;
 let c = a + b;
 // if we use "," then space will be added; if we use "+" then there will be no space
-//example of multiple arguments of console log
+//example of multiple arguments of console.log
 console.log("Addition of", a + " and " + b, "is:", c);
 // example of single arguments of console log
 
 console.log("Addition is:" + c);
+// use arithmatic operators inside console.log
+console.log("Addition of", a + " + " + b + " + ", "is:", a + b + c);
