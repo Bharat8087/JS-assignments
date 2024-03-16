b6dfd3ad440f6db068ce4096835a85dd362e157e
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-03-16 20:09:26 +0530

    multiple console log

--- addition.js
+++ addition.js
@@ -1,5 +1,9 @@
 let a = 5;
 let b = 7;
 let c = a + b;
+// if we use "," then space will be added; if we use "+" then there will be no space
+//example of multiple arguments of console log
+console.log("Addition of", a + " and " + b, "is:", c);
+// example of single arguments of console log
 
-console.log("Addition is:", c);
+console.log("Addition is:" + c);
